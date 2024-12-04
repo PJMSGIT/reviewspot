ffa17eaa1c6e4a388aef5ce17410affd0f5a23db
Pablo <pablojavier851@gmail.com>
2024-12-04 09:59:47 +0000

    Fixed Database
    Remeber to always import db to sql file before commiting...
    
    If you create new user, add the sql sentences to users.sql

--- reviewspotDB/GITCLONE README.txt
+++
@@ -1,1 +0,0 @@
-Directory reviewspotDB is the data folder for the MariaDB database we are using to install and manage the site.
---
+++ users.sql
@@ -0,0 +1,10 @@
+repair table mysql.db;
+drop database if exists `reviewspotdb`;
+
+CREATE DATABASE `reviewspotdb`;
+
+drop user if exists `reviewspotAdmin`@`localhost`;
+
+GRANT USAGE ON *.* TO `reviewspotAdmin`@`localhost` IDENTIFIED BY PASSWORD '*81F5E21E35407D884A6CD4A731AEBFB6AF209E1B';
+
+GRANT ALL PRIVILEGES ON `reviewspotdb`.* TO `reviewspotAdmin`@`localhost` WITH GRANT OPTION;
