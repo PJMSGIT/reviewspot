a1f3572924fb99da4369798ff721293d1c5a147d
Pablo <pablojavier851@gmail.com>
2024-12-10 13:50:02 +0000

    Fixed db import

--- update.bat
+++ update.bat
@@ -3,3 +3,5 @@ cd C:\xampp
 TYPE C:\xampp\htdocs\reviewspot\users.sql C:\xampp\htdocs\reviewspot\reviewspotDB.sql | mysql\bin\mysql -u root -P 3307 -p ""
 
 cd C:\xampp\htdocs\reviewspot
+
+timeout 5
--- users.sql
+++ users.sql
@@ -8,3 +8,5 @@ drop user if exists `reviewspotAdmin`@`localhost`;
 GRANT USAGE ON *.* TO `reviewspotAdmin`@`localhost` IDENTIFIED BY PASSWORD '*81F5E21E35407D884A6CD4A731AEBFB6AF209E1B';
 
 GRANT ALL PRIVILEGES ON `reviewspotdb`.* TO `reviewspotAdmin`@`localhost` WITH GRANT OPTION;
+
+use reviewspotdb;
